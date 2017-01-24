a9950b7fdb4c46e6be9f739051a20de180b6c7e7
PrestoXen <PrestoXen@cock.li>
2017-01-24 07:22:38 +0000

    Add gst-opera cloning instructions

---
+++ gst-opera_clone.bat
@@ -0,0 +1,36 @@
+@echo off
+git clone http://sourcecode.opera.com/gstreamer/gst-opera.git
+cd gst-opera
+
+rem clone sub-modules manually
+git clone http://sourcecode.opera.com/gstreamer/libogg.git
+git clone http://sourcecode.opera.com/gstreamer/libvorbis.git
+git clone http://sourcecode.opera.com/gstreamer/libtheora.git
+git clone http://sourcecode.opera.com/gstreamer/glib.git
+git clone http://sourcecode.opera.com/gstreamer/gstreamer.git
+git clone http://sourcecode.opera.com/gstreamer/gst-plugins-base.git
+git clone http://sourcecode.opera.com/gstreamer/gst-plugins-good.git
+
+rem gst-plugins-bad is borked, need to clone it a different way
+rem YOU NEED WGET INSTALLED FOR THIS TO WORK
+wget -r http://sourcecode.opera.com/gstreamer/gst-plugins-bad.git -P gst-opera-bad1.git
+git clone gst-opera-bad1.git/sourcecode.opera.com/gstreamer/gst-plugins-bad.git
+
+rem setup each sub-module repo to have correct code
+cd glib
+git checkout remotes/origin/2.20.5+OPERA
+
+cd ../gst-plugins-bad
+git checkout remotes/origin/RELEASE-0.10.19+OPERA
+
+cd ../gst-plugins-base
+git checkout remotes/origin/RELEASE-0.10.29+OPERA
+
+cd ../gst-plugins-good
+git checkout remotes/origin/RELEASE-0.10.23+OPERA
+
+cd ../gstreamer
+git checkout remotes/origin/RELEASE-0.10.29+OPERA
+
+rem done!
+echo gst-opera clone finished!
